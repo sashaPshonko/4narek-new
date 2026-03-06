096e6b51e8b77790e56f0ee1095104e899ac93db
sashaPshonko <161281665+sashaPshonko@users.noreply.github.com>
2026-03-06 20:42:51 +0500

    dsasda

--- main.go
+++ main.go
@@ -1007,7 +1007,7 @@ func adjustPrice(item string) {
 		if sales > cfg.NormalSales {
 			salesLeader = sales
 		}
-		if float64(totalStock) > float64(salesLeader)*2 {
+		if float64(totalStock) > float64(salesLeader)*3 {
 			newPrice -= cfg.PriceStep
 			if newPrice < cfg.MinPrice {
 				newPrice = cfg.MinPrice
