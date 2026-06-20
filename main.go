11769e0ba802400de1af94a060e840835b6c7dd2
sashaPshonko <privetnitron@gmail.com>
2026-06-20 15:07:14 +0500

    xcc

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ var (
 	upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
 	}
-	// tgBot *bot.Bot
+	tgBot *bot.Bot
 )
 
 var (
@@ -180,7 +180,7 @@ func main() {
 		log.Printf("Error creating bot: %v", err)
 		os.Exit(1)
 	}
-	// tgBot = b
+	tgBot = b
 
 	// Инициализация данных
 	data.Prices = make(map[string]int)
@@ -760,38 +760,38 @@ func updateTelegramMessageWithoutLocks(prices, buyStats, sellStats map[string]in
 
 	ctx := context.Background()
 
-	// var newMessageID int
-	// if messageID == 0 {
-		// msg, err := tgBot.SendMessage(ctx, &bot.SendMessageParams{
-		// 	ChatID: chatID,
-		// 	Text:   msgText,
-		// })
-		// if err != nil {
-		// 	log.Printf("[Telegram error] Не удалось отправить новое сообщение: %v", err)
-		// 	return
-		// }
-	// 	newMessageID = msg.ID
-	// } else {
-	// 	_, err := tgBot.EditMessageText(ctx, &bot.EditMessageTextParams{
-	// 		ChatID:    chatID,
-	// 		MessageID: messageID,
-	// 		Text:      msgText,
-	// 	})
-	// 	if err != nil {
-	// 		log.Printf("[Telegram error] Не удалось обновить сообщение: %v", err)
+	var newMessageID int
+	if messageID == 0 {
+		msg, err := tgBot.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: chatID,
+			Text:   msgText,
+		})
+		if err != nil {
+			log.Printf("[Telegram error] Не удалось отправить новое сообщение: %v", err)
+			return
+		}
+		newMessageID = msg.ID
+	} else {
+		_, err := tgBot.EditMessageText(ctx, &bot.EditMessageTextParams{
+			ChatID:    chatID,
+			MessageID: messageID,
+			Text:      msgText,
+		})
+		if err != nil {
+			log.Printf("[Telegram error] Не удалось обновить сообщение: %v", err)
 
-	// 		msg, sendErr := tgBot.SendMessage(ctx, &bot.SendMessageParams{
-	// 			ChatID: chatID,
-	// 			Text:   msgText,
-	// 		})
-	// 		if sendErr == nil {
-	// 			newMessageID = msg.ID
-	// 		} else {
-	// 			log.Printf("[Telegram error] Повторная отправка тоже не удалась: %v", sendErr)
-	// 			return
-	// 		}
-	// 	}
-	// }
+			msg, sendErr := tgBot.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: chatID,
+				Text:   msgText,
+			})
+			if sendErr == nil {
+				newMessageID = msg.ID
+			} else {
+				log.Printf("[Telegram error] Повторная отправка тоже не удалась: %v", sendErr)
+				return
+			}
+		}
+	}
 
 	if newMessageID != 0 {
 		mutex.Lock()
@@ -1189,14 +1189,14 @@ func sendIntervalStatsToTelegram(item string, start, end time.Time, actualSales,
 	)
 
     ctx := context.Background()
-    // _, err := tgBot.SendMessage(ctx, &bot.SendMessageParams{
-    //     ChatID:    -4633184325,
-    //     Text:      msg,
-    //     ParseMode: "Markdown",
-    // })
-    // if err != nil {
-    //     log.Printf("[Telegram] Ошибка при отправке интервал-статы: %v", err)
-    // }
+    _, err := tgBot.SendMessage(ctx, &bot.SendMessageParams{
+        ChatID:    -4633184325,
+        Text:      msg,
+        ParseMode: "Markdown",
+    })
+    if err != nil {
+        log.Printf("[Telegram] Ошибка при отправке интервал-статы: %v", err)
+    }
 
 	plainLog := fmt.Sprintf(
 		"%s [%s → %s] %s | Покупки: %.0f | Продажи: %.0f/%.0f | Цена: %d→%d | На руках: %d | Онлайн: %d\n",
